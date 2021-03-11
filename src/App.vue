76c8d4ef6556686646bdb9b5299df98df281d42f
Franco <francorsr98@gmail.com>
2021-03-11 12:20:16 -0600

    NavBar component was added to the main view

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <NavBar />
   </div>
-  <router-view/>
+  <router-view />
 </template>
+
+<script>
+  import NavBar from '@/components/NavBar.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      NavBar
+    }  
+  }
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/">Inicio</router-link></li>
+      <li><router-link to="/aboutme">About me</router-link></li>
+      <li><router-link to="/portafolio">Portafolio</router-link></li>
+      <li><router-link to="/contactame">Contactame</router-link></li>  
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar'
+  }
+</script>
